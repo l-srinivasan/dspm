a375d1901d0ea47fb4076ba90bbc278f2b99b202
Leela Srinivasan <leelasrinivasan07@gmail.com>
2024-10-24 15:12:40 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 17 12:09:56 2024
+@author: Leela Srinivasan
+
+Note: resection mask should be contained in 'resection_dir' with filename rsxn.msk.nii.
+
+"""
+
+config = {}
+
+config['freesurfer_dir'] = 'input_freesurfer_path'
+config['resection_dir'] = 'input_fresection_mask_directory_path'
+config['working_dir_prefix'] = 'input_working_dir_prefix'
+config['code_dir'] = 'input_path_to_downloaded_codebase'
+config['resource_dir'] = 'input_path_to_downloaded_resources'
